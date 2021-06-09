9dbd53b80db860a3d43f38dcbc5bebda91eb94cd
mova <valeriy.molchanov.77@gmail.com>
2021-06-09 15:10:02 +0300

    @ngrx/store added

--- src/mova/apps/mova/src/app/app.module.ts
+++ src/mova/apps/mova/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
