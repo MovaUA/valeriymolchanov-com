91cc8ce57a7bf17f3aee0793f405ce7848f8a4d2
mova <valeriy.molchanov.77@gmail.com>
2021-06-11 00:31:59 +0300

    cleanup

--- src/mova/apps/hp/src/app/app.component.spec.ts
+++ src/mova/apps/hp/src/app/app.component.spec.ts
@@ -19,13 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('hp');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to hp!'
-    );
-  });
 });
