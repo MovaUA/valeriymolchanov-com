66e6682763f576c7f87b5b1a9e05e80ca32137d0
mova <valeriy.molchanov.77@gmail.com>
2021-06-11 16:58:49 +0300

    AppComponent.title removed

--- src/mova/apps/hp/src/app/app.component.spec.ts
+++ src/mova/apps/hp/src/app/app.component.spec.ts
@@ -13,10 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'hp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('hp');
-  });
 });
