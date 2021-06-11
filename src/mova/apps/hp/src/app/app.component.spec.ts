e829637a37cfeb8cd81d074d8e2e7a92e1cb3224
mova <valeriy.molchanov.77@gmail.com>
2021-06-11 20:25:39 +0300

    tests fixed

--- src/mova/apps/hp/src/app/app.component.spec.ts
+++ src/mova/apps/hp/src/app/app.component.spec.ts
@@ -1,10 +1,29 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, NavComponent],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+      ],
     }).compileComponents();
   });
 
