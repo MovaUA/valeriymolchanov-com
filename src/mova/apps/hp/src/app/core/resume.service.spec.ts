47a5ca188a89455df50b330e6919b8b805cf54ed
mova <valeriy.molchanov.77@gmail.com>
2021-07-03 23:41:01 +0300

    resume.service.spec

--- src/mova/apps/hp/src/app/core/resume.service.spec.ts
+++ src/mova/apps/hp/src/app/core/resume.service.spec.ts
@@ -1,17 +1,45 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ResumeService } from './resume.service';
 
 describe('ResumeService', () => {
+  let httpTestingController: HttpTestingController;
   let service: ResumeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.inject(ResumeService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('get() returns Resume', () => {
+    const body = `
+name: One
+surname: Two
+    `;
+
+    service.get().subscribe((resume) => {
+      expect(resume.name).toEqual('One');
+      expect(resume.surname).toEqual('Two');
+    });
+
+    const req = httpTestingController.expectOne('assets/resume.yaml');
+
+    req.flush(body);
   });
 });
