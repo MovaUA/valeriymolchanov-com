f3b59a4bee7bb777e9d4b024554428853ab9e65b
mova <valeriy.molchanov.77@gmail.com>
2021-07-08 09:30:03 +0300

    nav draft

--- src/mova/apps/hp/src/app/features/nav/nav.module.ts
+++ src/mova/apps/hp/src/app/features/nav/nav.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { NavRoutingModule } from './nav-routing.module';
-import { NavComponent } from './componets/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+import { NavRoutingModule } from './nav-routing.module';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [NavComponent],
